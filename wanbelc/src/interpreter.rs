1464f9bf7b7f70e7237a1a254bf2b62eb69d7080
lesmart13 <paradisesoft.png@gmail.com>
2025-04-15 10:01:21 +1000

    update interpreter.rs - let x = value -> variable declaration, get x -> retrieving stored values

--- wanbelc/src/interpreter.rs
+++ wanbelc/src/interpreter.rs
@@ -1,9 +1,25 @@
+use std::collections::HashMap;
 use crate::ast::Expr;
 
 pub fn eval(expr: Expr) {
+    use std::cell::RefCell;
+    thread_local! {
+        static ENV: RefCell<HashMap<String, String>> = RefCell::new(HashMap::new());
+    }
+
     match expr {
-        Expr::Say(msg) => {
-            println!("{}", msg),
-        }
+        Expr::Say(msg) => println!("{}", msg),
+        Expr::Let(var, val) => { 
+            ENV.with(|env| env.borrow_mut().insert(var.clone(), val.clone()));
+            println!("[stored] {} = \"{}"\"", var, val);
+        },
+        Expr::Get(var) => {
+            ENV.with(|env| {
+                match env.borrow().get(&var) {
+                    Some(val) => println!("{}", val),
+                    None => println!("[undefined variable] {}", var),
+                }
+            });
+        },
     }
 }
